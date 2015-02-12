d82dc324ff2bfcbd49cb2e0f98d54e999432a327
devsdmf <devsdmf@gmail.com>
2015-02-12 21:53:25 -0200

    Updated travis yml for the improved tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,3 +2,17 @@
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 $loader->add('DataMonkey\Tests', __DIR__);
+
+if((bool)getenv('TRAVIS_ENV')) {
+    define('DATABASE_DRIVER','pdo_mysql');
+    define('DATABASE_HOST','localhost');
+    define('DATABASE_USER','root');
+    define('DATABASE_PASS','');
+    define('DATABASE_NAME','datamonkey_test');
+} else {
+    define('DATABASE_DRIVER','pdo_mysql');
+    define('DATABASE_HOST','localhost');
+    define('DATABASE_USER','root');
+    define('DATABASE_PASS','');
+    define('DATABASE_NAME','datamonkey_test');
+}
